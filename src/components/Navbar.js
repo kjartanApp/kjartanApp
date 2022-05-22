ab86799c9a3829aa8f7c5c16f934b5fca28783f5
kjartanApp <105993442+kjartanApp@users.noreply.github.com>
2022-05-22 02:47:35 -0400

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <div className="bd-example">
+      <nav className="navbar navbar-expand-lg navbar-dark">
+          <Link to="/" className="navbar-brand">
+              <img className="img-logo-kjartan" src={`${process.env.PUBLIC_URL}/assets/images/logo-small.png`} alt="logo"/>
+          </Link>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Dropdown
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </li>
+            </ul>
+          </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
